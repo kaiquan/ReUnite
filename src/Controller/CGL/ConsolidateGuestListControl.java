11be2c75600a4f9ab4f30993e11f062765e3b2bf
Ameen <anniyan123456789@hotmail.com>
2012-08-03 16:05:20 +0800

    update

--- src/Controller/CGL/ConsolidateGuestListControl.java
+++ src/Controller/CGL/ConsolidateGuestListControl.java
@@ -20,109 +20,153 @@ public ArrayList<Event> processExpiredInvitation(){
 		return e1.RETRIEVE_EVENT_RECORDS();
 	}
 	
-public ArrayList<String> requestSelectedEventDetails(String eventName){
+public ArrayList<String> requestSelectedEventDetails(String eventName)
+{	
+	//Creates Ballroom Object
+	//Creates ArrayList of String 
+	//get the ballroom details and adds it to the arrayList
 	Ballroom b1 = new Ballroom();
-	ArrayList<String> ballroom= new ArrayList<String>();
+	ArrayList<Ballroom> ballroom= new ArrayList<Ballroom>();
 	ballroom=b1.GET_BALLROOM_DETAILS(eventName);
 	
-	ArrayList<String> entertainment = new ArrayList<String>();
+	
+	//Creates Entertainment Object
+	//Creates ArrayList of String 
+	//get the entertainment details and adds it to the arrayList
+	ArrayList<Entertainment> entertainment = new ArrayList<Entertainment>();
 	Entertainment e1 = new Entertainment();
 	entertainment=e1.GET_ENTERTAINMENT_PRICE(eventName);
 	
-	ArrayList<String> event = new ArrayList<String>();
+	//Creates event Object
+	//Creates ArrayList of String 
+	//get the event details and adds it to the arrayList
+	ArrayList<Event> event = new ArrayList<Event>();
 	Event ev1 = new Event();
 	event=ev1.GET_EVENT_DETAILS(eventName);
 	
-	ArrayList<String> guest = new ArrayList<String>();
+	//Creates guest Object
+	//Creates ArrayList of String 
+	//get the number of guests and adds it to the arrayList
+	ArrayList<Guest> guest = new ArrayList<Guest>();
 	Guest g1= new Guest();
 	guest=g1.getNumberOfGuests(eventName);
 	
-	ArrayList<String> meal = new ArrayList<String>();
+	//Creates meal Object
+	//Creates ArrayList of String 
+	//get the meal details and adds it to the arrayList
+	ArrayList<Meal> meal = new ArrayList<Meal>();
 	Meal m1 = new Meal();
-	try{
-	meal=m1.GET_MEAL_PRICE(eventName);
+	
+	//Creates facility Object
+	//Creates ArrayList of String 
+	//get the facilty name
+	ArrayList<Facility> facilty = new ArrayList<Facility>();
+	Facility f1 = new Facility();
+	facilty=f1.GET_FACILITY(eventName);
+	
+	//Creates meal Object
+	//Creates ArrayList of String 
+	//get the meal details and adds it to the arrayList
+	ArrayList<Package> pkg  = new ArrayList<Package>();
+	Package p1 = new Package();
+	pkg=p1.GET_PACKAGE_DISCOUNT(eventName);
+	
+	//If no meal option is selected it add 0 into the arraylist instead
+	try
+	{
+		meal=m1.GET_MEAL_PRICE(eventName);
 	}
 	
-	catch(Exception ex){
-		meal.add("0");
+	catch(Exception ex)
+	{
+		meal.get(0).setMealFinalPrice(0);
 	}
 	
 	
-	
+	//get the ballroomFinalPrice & the ballroom name based on the arraylist 
+	//we create earlier
 	double ballroomFinalPrice;
 	String ballroomName;
-	ArrayList<String> combined = new ArrayList<String>();
-	try {
-	Scanner sc = new Scanner(ballroom.get(0));
-	sc.useDelimiter(",");
-	ballroomName=sc.next();
-	ballroomFinalPrice=sc.nextDouble();
+	
+	try 
+	{
+		ballroomName=ballroom.get(0).getBallroomName();
+		ballroomFinalPrice=(ballroom.get(0).getBallroomFinalPrice());
 	}
 	catch (Exception ex)
 	{
 		ballroomFinalPrice = 0;
-		ballroomName = "None";
+		ballroomName="None";
+		
 	}
+	
+	//get the entertainment based on the arraylist 
+	//we create earlier		
 	double entertainmentPrice;
 	
-	try{
-	Scanner sc1 = new Scanner(entertainment.get(0));
-	entertainmentPrice=sc1.nextDouble();
-	
+	try
+	{
+		
+		entertainmentPrice=entertainment.get(0).getEntertainmentFinalPrice();
 	}
 	
-	catch(Exception ex){
+	catch(Exception ex)
+	{
 		 entertainmentPrice=0;
 	}
 	
-	Scanner sc2 = new Scanner(event.get(0));
-	sc2.useDelimiter(",");
-	String eventTime=sc2.next();
-	String eventDate=sc2.next();
-	String eventStatus=sc2.next();
-	String eventDescription=sc2.next();
 	
-	Scanner sc3 = new Scanner(guest.get(0));
+	//get the EventTime,EventDate,EventStatus,EventDescription based on the arraylist 
+	//we create earlier
+	String eventTime=event.get(0).getEventTime();
+	System.out.println(event.get(0).getEventTime());
+	String eventDate=event.get(0).getEventDate();
+	String eventStatus=event.get(0).getEventStatus();
+	String eventDescription=event.get(0).getEventDescription();
 	
-	String guestCount=sc3.next();
 	
+	//get the number of guests based on the arraylist 
+	//we create earlier
+	
+	String guestCount=guest.get(0).getNoOfGuests();
+	
+	//get the total meal price based on the different options selected by guests based on the arraylist 
+	//we create earlier
 	double mealPrice;
-	try{
-	Scanner sc4 = new Scanner(meal.get(0));
-	sc4.useDelimiter(",");
-	mealPrice =sc4.nextDouble();
+	try
+	{
+		
+		mealPrice =meal.get(0).getMealFinalPrice();
 	}
 	
-	catch(Exception ex){
+	catch(Exception ex)
+	{
 		mealPrice=0;
 	}
 	
-	Facility f1 = new Facility();
-	String facility=f1.GET_FACILITY(eventName).get(0);
 	
+	//get the facility name based on the arraylist 
+	//we create earlier
+	String facility=facilty.get(0).getFacilityName();
 	
+	//get the package discount based on the arraylist 
+	//we create earlier
 	Package p = new Package();
-	String pkgDiscount=p.GET_PACKAGE_DISCOUNT(eventName).get(0);
-	double packageDiscount=Double.parseDouble(pkgDiscount);
 	
+	double packageDiscount=pkg.get(0).getPackageDiscount();
+	
+	//Calculates total payable amount
 	double totalPrice=ballroomFinalPrice+entertainmentPrice+mealPrice-packageDiscount;
 	
-
+	//Create an arrayList to store all the details which will passed to the form
+	ArrayList<String> combined = new ArrayList<String>();
 	
 	
-	
-	
-	
-	for(int i=0;i<meal.size();i++){
-		String combine=ballroomName+","+eventTime+","+eventDate+","+eventStatus+","+eventDescription+","+guestCount+","+totalPrice+","+ballroomFinalPrice+","+entertainmentPrice+","+mealPrice+","+facility+","+pkgDiscount;
+	for(int i=0;i<1;i++){
+		String combine=ballroomName+","+eventTime+","+eventDate+","+eventStatus+","+eventDescription+","+guestCount+","+totalPrice+","+ballroomFinalPrice+","+entertainmentPrice+","+mealPrice+","+facility+","+packageDiscount;
 		combined.add(combine);
 	}
-	
-	
-	
-	System.out.println(combined.get(0));
-	
-	
+		
 	return combined;
 }
 
